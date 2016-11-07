2f234a3e13dd0dfc0c5b163d38dbc8922bd07733
Jacob Bieker <jacob@bieker.tech>
2016-11-07 18:38:08 +0100

    Reads in FITs files
    Also extracts the three necessary arrays for Cross Correlation Function, but do not have the initial wavelength yet to finish the conversion to km/s

--- radial_velocity.py
+++ radial_velocity.py
@@ -1,22 +1,53 @@
 import os
 import glob
-from astropy.table import Table, vstack
 from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Read in the data
 def read_files(directory):
+
+    def convert_to_km(wavelength):
+        wavelength = 1.0 - wavelength
+        converted = np.array([])
+        r = 0.5/2.997924e5  # <- For .5km/s resolution
+
+        return converted
+
+
     filenames = glob.glob(os.path.join(directory, "*.fits"))
     print(filenames)
-    finished_table = Table()
     for data_file in filenames:
-        table = Table.read(data_file, format="fits")
-        finished_table = vstack(finished_table, table)
-    print(finished_table)
+        table = fits.open(data_file)
+        data = table[0].data
+        header = table[0].header
+        print(data)
+        print(repr(header))
+        print(data.shape)
+        # Going through each of the 5 data sets
+        '''
+         1: the extracted spectrum (2048x51)
+         2: the summed spectrum
+         3: the blaze function (2048x51)
+         4: the wavelength (2048x51), closest in time before the science exposure
+         5: the wavelength (2048x51), closest in time after the science exposure.
 
-#read_files(os.path.join("data"))
+         r = 0.5/2.997924d5  <- For .5km/s resolution
+         w1 = 5187.0 * (1.d0+r)^dindgen(1024)
 
-hdulist = fits.open("/home/jacob/Development/Aarhus-Radial-Velocity/data/s1_2015-04-19T21-46-20_ext.fits")
-print(hdulist.info())
-data = hdulist[0].data
-print(data)
+         Where r is the inverse spectroscopic resolution power R
+
+        '''
+        # extracted spectrum
+        extracted_spectrum = data[0]
+        before_spectrum = data[3]
+        after_spectrum = data[4]
+        converted_extracted = convert_to_km(extracted_spectrum)
+        converted_before = convert_to_km(before_spectrum)
+        converted_after = convert_to_km(after_spectrum)
+
+
+read_files(os.path.join("data"))
+
+
