5bf5384972403db6d76f45edfd5a92c0918105ef
Jacob Bieker <jacob@bieker.tech>
2016-11-24 10:57:17 +0100

    Plotting works
    Now just need to convert to km/s before doing a cross-correlation, still not sure the basic steps in wavelength

--- radial_velocity.py
+++ radial_velocity.py
@@ -4,6 +4,15 @@ from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
 
+def cross_correlation(extracted, before, after):
+    """
+
+    :param extracted: A list of arrays of extracted spectrum converted to km/s
+    :param before: A list of arrays of spectrum taken right before the extracted ones in km/s
+    :param after: A list of arrays of spectrum taken right before the extracted ones in km/s
+    :return: Arrays with cross-correlated data
+    """
+
 
 # Read in the data
 def read_files(directory):
@@ -14,6 +23,12 @@ def read_files(directory):
         r = 0.5/2.997924e5  # <- For .5km/s resolution
 
         return converted
+
+    def plot_wavelength(spectrum, wavelength):
+        print(spectrum)
+        for index, element in enumerate(spectrum):
+            plt.plot(wavelength[index], element)
+            plt.show()
 
 
     filenames = glob.glob(os.path.join(directory, "*.fits"))
@@ -43,6 +58,8 @@ def read_files(directory):
         extracted_spectrum = data[0]
         before_spectrum = data[3]
         after_spectrum = data[4]
+        plot_wavelength(extracted_spectrum, before_spectrum)
+        plot_wavelength(extracted_spectrum, after_spectrum)
         converted_extracted = convert_to_km(extracted_spectrum)
         converted_before = convert_to_km(before_spectrum)
         converted_after = convert_to_km(after_spectrum)
